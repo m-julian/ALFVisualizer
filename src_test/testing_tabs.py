7eb644a0b2d68e4af632ce8080e2a1e7bfa58d80
m-julian <52214154+m-julian@users.noreply.github.com>
2021-12-14 01:33:00 +0200

    figured out how to add tabs

---
+++ src_test/testing_tabs.py
@@ -0,0 +1,60 @@
+from PyQt5.QtWidgets import QWidget
+from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtWidgets import QMainWindow
+import pyvista as pv
+from pyvistaqt import QtInteractor
+from qtpy import uic
+import os
+import sys
+from pathlib import Path
+
+# Setting the Qt bindings for QtPy 5, change if using Pyside 2
+# os.environ["QT_API"] = "pyside2"
+# http://qtdocs.pyvista.org/usage.html
+os.environ["QT_API"] = "pyqt5"
+
+
+class Button(QtWidgets.QPushButton):
+
+    def __init__(self, name):
+
+        super().__init__()
+        self.setText(name)
+
+
+class VisualizationWindow(QMainWindow):
+    """ handles GUI and connects user commands with what to plot on pyvista plot
+    see https://www.youtube.com/channel/UCj7i-mmOjLV17YTPIrCPkog videos for info on using Qt with python """
+
+    def __init__(self):
+
+        super().__init__()
+
+        # Setup for ui
+        ui_path = os.path.join(".", "testing_tabs.ui")
+        Ui_MainWindow, _ = uic.loadUiType(ui_path)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.tabWidget.currentChanged.connect(self.add_tab)
+        self.ui.tabWidget.setCurrentIndex(0)
+        self.ui.tabWidget.setTabsClosable(True)
+        self.ui.tabWidget.tabBar().tabButton(1, QtWidgets.QTabBar.RightSide).resize(0, 0)
+
+    def add_tab(self):
+
+        current_tab = self.sender().currentIndex()
+        if current_tab == self.sender().count()-1:
+            self.ui.tabWidget.insertTab(current_tab, QtWidgets.QWidget(), f"test{current_tab}")
+            self.ui.tabWidget.setCurrentIndex(current_tab)
+            layout = QtWidgets.QVBoxLayout()
+            layout.addWidget(Button(str(current_tab)))
+            self.ui.tabWidget.currentWidget().setLayout(layout)
+
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+    ex = VisualizationWindow()
+    ex.show()
+    app.exec_()
